95bfc9a706bdedeee02dc75b52494eb02d8fa296
Yura <scsspb@mail.ru>
2022-06-27 10:50:23 +0300

    add Exemple 009 добавили метод

--- Exemple009_Sem2/Program.cs
+++ Exemple009_Sem2/Program.cs
@@ -6,9 +6,21 @@
 int rndNum = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
 
-int firstDigit = rndNum / 10;
-int secondDigit = rndNum % 10;
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
 // if(firstDigit > secondDigit) maxDigit = firstDigit;
 // else maxDigit = secondDigit;
-Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+// Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+
+int MaxDigit (int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+  
+}
+int maxDigit = MaxDigit(rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры одиннаковые";
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
