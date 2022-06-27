fadd211699bce2f776903ac9ef9bd90bb0cc30f3
Yura <scsspb@mail.ru>
2022-06-27 12:22:11 +0300

    add Exemple 012,14,16

--- Exemple011_Samost/Program.cs
+++ Exemple011_Samost/Program.cs
@@ -8,3 +8,24 @@ int second = (rndNumer / 100) * 10;
 int third = rndNumer % 10;
 int sum = second + third;
 Console.WriteLine(sum);
+// // 11. Напишите программу, которая выводит
+// // случайное трёхзначное число и удаляет
+// // вторую цифру этого числа.
+// // 456 -> 46
+// // 782 -> 72
+// // 918 -> 98
+
+// var rndNum = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное трехзначное число: {rndNum}");
+
+// int CutMid (int num)
+// {
+//     int firstDig = num / 100 * 10;
+//     int secondDig = num % 10;
+//     return firstDig + secondDig;
+// }
+// int result = CutMid(rndNum);
+
+// Console.WriteLine($"Полученное двухзначное число(через метод): {result}");
+
+// Console.WriteLine($"Полученное двухзначное число: {rndNum / 100 * 10 + rndNum % 10}");
---
+++ Exemple012/Program.cs
@@ -0,0 +1,19 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int remainder = num1 % num2;
+bool Method(int num1, int num2){
+if(num1%num2 == 0) return true;
+ return false;
+}
+bool met = Metod(num1, num2);
+string result = Metod == true ? "кратно" : "не кратно, остаток {remainder}";
+
+
